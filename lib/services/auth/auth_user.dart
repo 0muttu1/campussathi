eed83131411c49dec142d99fcca4eb20ea390c46
Chanvirappa Prachande <chanvirappa1@gmail.com>
2023-02-20 00:56:28 +0530

    added services

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,12 @@
+import "package:firebase_auth/firebase_auth.dart" show User;
+import "package:flutter/material.dart";
+
+//creating a class for user to verify the user email
+
+@immutable //cant be changed
+class AuthUser {
+  final bool isEmailVerified;
+  const AuthUser(this.isEmailVerified);
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+}
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,7 +2,6 @@
 import 'package:campussathi/constants/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import '../utilities/show_error_dialog.dart';
 
